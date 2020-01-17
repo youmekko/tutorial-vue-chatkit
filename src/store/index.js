5047a4c654e6132d5ef0272ce4a2508536e8ca44
youmekko <youmekko.o@gmail.com>
2020-01-17 09:48:01 +0900

    storage 셋팅

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,24 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import VuexPersistence from './mutations'
+import actions from './actions'
 
 Vue.use(Vuex)
+
+const debug = process.env.NODE_ENV ! == 'production';
+
+const vuexLocal = new VuexPersistence({
+  storage: window.localStorage
+})
 
 export default new Vuex.Store({
   state: {
   },
-  mutations: {
+  mutations,
+  actions,
+  getters :{
+
   },
-  actions: {
-  },
-  modules: {
-  }
+  plugins: [vuexLocal.plugins],
+  strict: debug
 })
